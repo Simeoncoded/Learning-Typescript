3e24f9c8bed9ad871be44bcd44cb5b3a7f4b3207
Simeoncoded <simeonolawore07@gmail.com>
2025-07-28 12:32:08 -0400

    initial

---
+++ index.ts
@@ -0,0 +1,9 @@
+//const bootupMessage : string = "Starting support";
+
+//const bootupMessage : boolean = true;
+
+//const bootupMessage = true;
+
+const supportAiPort : number = 3000;    
+
+console.log(supportAiPort);
